f337f7166dd80480e8ccfacc246940f9a85a2601
Catch <catchlife6@163.com>
2018-09-20 09:46:25 +0800

    update

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 git add .
-git commit -m 'update'
+git commit -m "update"
 git push origin master
 vuepress build .
-pause
--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 cd ./.vuepress/dist
 git init
 git add -A
-git commit -m 'update'
+git commit -m "update"
 git push -f git@gitee.com:catchlife/effective-java-notes.git master:gh-pages
