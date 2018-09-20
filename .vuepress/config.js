983e5cd4e9ccf6fea4b276166477e4a945b8d5ac
Catch <catchlife6@163.com>
2018-09-20 18:36:47 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,61 +1,55 @@
 var fs = require('fs')
 var path = require('path')
 
-var themeConf = (function name(params) {
-  // 处理目录
-  var titles = [
-    '第1章 引言',
-    '第2章 创建和销毁对象',
-    '第3章 对于所有对象都通用的方法',
-    '第4章 类和接口',
-    '第5章 泛型',
-    '第6章 枚举和注解'
-  ]
+// 处理目录
+var titles = [
+  '第1章 引言',
+  '第2章 创建和销毁对象',
+  '第3章 对于所有对象都通用的方法',
+  '第4章 类和接口',
+  '第5章 泛型',
+  '第6章 枚举和注解'
+]
 
-  var rootPath = path.resolve('./')
+var rootPath = path.resolve('./')
 
-  var excludeFileNames = ['.git', '.vuepress', 'node_modules']
+var excludeFileNames = ['.git', '.vuepress', 'node_modules']
 
-  var Contents = []
+var Contents = []
 
-  var i = 0
-  var its = []
+var i = 0
+var its = []
 
-  var dirs = fs.readdirSync(rootPath)
+var dirs = fs.readdirSync(rootPath)
 
-  dirs.forEach(dirName => {
-    if (excludeFileNames.indexOf(dirName) === -1) {
-      var realDir = path.join(rootPath, dirName)
-      var stats = fs.statSync(realDir)
-      if (stats.isDirectory()) {
-        its = []
-        var docs = fs.readdirSync(realDir)
-        docs.forEach(docName => {
-          its.push(docName.split('.')[0])
-        })
-        Contents.push({
-          text: titles[i],
-          dir: dirName,
-          items: its
-        })
-        i++
-      }
+dirs.forEach(dirName => {
+  if (excludeFileNames.indexOf(dirName) === -1) {
+    var realDir = path.join(rootPath, dirName)
+    var stats = fs.statSync(realDir)
+    if (stats.isDirectory()) {
+      its = []
+      var docs = fs.readdirSync(realDir)
+      docs.forEach(docName => {
+        its.push(docName.split('.')[0])
+      })
+      Contents.push({
+        text: titles[i],
+        dir: dirName,
+        items: its
+      })
+      i++
     }
-  })
-  var themeItems = []
-  var themeSidebar = {}
-  Contents.forEach(content => {
-    themeItems.push({
-      text: content.text,
-      link: `/${content.dir}/${content.items[0]}`
-    })
-    themeSidebar[`/${content.dir}/`] = content.items
-  })
-  return {
-    themeItems,
-    themeSidebar
   }
-})()
+})
+var themeItems = []
+var themeSidebar = {}
+Contents.forEach(content => {
+  themeItems.push({
+    text: content.text,
+    link: `/${content.dir}/${content.items[0]}`
+  })
+  themeSidebar[`/${content.dir}/`] = content.items
+})
 
 module.exports = {
   title: 'Effective Java Notes',
@@ -76,7 +70,7 @@ module.exports = {
     nav: [
       {
         text: '章节',
-        items: themeConf.themeItems
+        items: themeItems
       }
     ],
     serviceWorker: {
@@ -86,7 +80,7 @@ module.exports = {
       }
     },
     displayAllHeaders: true,
-    sidebar: themeConf.themeSidebar,
+    sidebar: themeSidebar,
     lastUpdated: '上次更新',
     repo: 'CatchLife/effective-java-notes',
     repoLabel: 'GitHub',
