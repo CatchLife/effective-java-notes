37754a1a120c79094760b8d01aa66bea05ff06f2
Catch <catchlife6@163.com>
2018-09-20 15:27:08 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,24 +1,58 @@
-var Contents = require('../Contents')
+var fs = require('fs')
+var path = require('path')
 
-const info = (function genContents() {
-  let items = []
-  let sidebar = {}
+var themeConf = function name(params) {
+  // 处理目录
+  var titles = ['引言', '创建和销毁对象']
+
+  var rootPath = path.resolve('./');
+
+  var excludeFileNames = ['.git', '.vuepress', 'node_modules']
+
+  var Contents = []
+
+  var i = 0;
+  var its = []
+
+  var dirs = fs.readdirSync(rootPath)
+
+  dirs.forEach(dirName => {
+    if (excludeFileNames.indexOf(dirName) === -1) {
+      var realDir = path.join(rootPath, dirName);
+      var stats = fs.statSync(realDir)
+      if (stats.isDirectory()) {
+        its = []
+        var docs = fs.readdirSync(realDir)
+        docs.forEach(docName => {
+          its.push(docName.split('.')[0])
+        })
+        Contents.push({
+          text: titles[i],
+          dir: dirName,
+          items: its
+        })
+        i++;
+      }
+    }
+  })
+  var themeItems = []
+  var themeSidebar = {}
   Contents.forEach(content => {
-    items.push({
+    themeItems.push({
       text: content.text,
       link: `/${content.dir}/${content.items[0]}`
     })
-    sidebar[`/${content.dir}/`] = content.items
+    themeSidebar[`/${content.dir}/`] = content.items
   })
   return {
-    items,
-    sidebar
+    themeItems,
+    themeSidebar
   }
-})()
+}()
 
 module.exports = {
-  title: 'effective java notes',
-  description: 'effective java 阅读笔记',
+  title: 'Effective Java Notes',
+  description: 'Effective Java 阅读笔记',
   port: 9600,
   base: '/effective-java-notes/',
   head: [
@@ -32,12 +66,10 @@ module.exports = {
   ],
   serviceWorker: true,
   themeConfig: {
-    nav: [
-      {
-        text: '章节',
-        items: info.items
-      }
-    ],
+    nav: [{
+      text: '章节',
+      items: themeConf.themeItems
+    }],
     serviceWorker: {
       updatePopup: {
         message: '发现新内容可用',
@@ -45,7 +77,7 @@ module.exports = {
       }
     },
     displayAllHeaders: true,
-    sidebar: info.sidebar,
+    sidebar: themeConf.themeSidebar,
     lastUpdated: '上次更新',
     repo: 'CatchLife/effective-java-notes',
     repoLabel: 'GitHub',
--- deploy.bat
+++
@@ -1,5 +0,0 @@
-cd .vuepress/dist
-git init
-git add -A
-git commit -m "update"
-git push -f git@github.com:CatchLife/effective-java-notes.git master:gh-pages
